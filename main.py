701f786180f25d12d6a20b8e9ae2b1db730521d2
heymynameiskai <github@ac.k41.me>
2022-05-25 14:52:07 +0200

    transferred to pycharm

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import api
 import pdf
+
+
 # import pprint
 # pp = pprint.PrettyPrinter(indent=4)
-
-
-
 
 def tablePrintHits(playlist_id):
     # 1st: get basic attributes of playlist, like name
@@ -14,14 +13,14 @@ def tablePrintHits(playlist_id):
     # check if API call worked
     if playlist_attributes_status == 200:
         playlist_name = playlist_attributes['name']
-        playlist_url = "https://cast.itunes.uni-muenchen.de/vod/playlists/"+playlist_id+".html"
+        playlist_url = "https://cast.itunes.uni-muenchen.de/vod/playlists/" + playlist_id + ".html"
         print("\n")
         print(playlist_name)
         print(playlist_url)
         print("\n")
     else:
-        print("Error while fetching API. Calling api.getPlaylistAttributes("+playlist_id+") returned HTTP-Status-Code: "+playlist_attributes_status)
-
+        print(
+            "Error while fetching API. Calling api.getPlaylistAttributes(" + playlist_id + ") returned HTTP-Status-Code: " + playlist_attributes_status)
 
     # 2nd: get number of hits for each video in playlist
     playlist_hits_status = api.getStatisticsByPlaylist(playlist_id)['status']
@@ -33,11 +32,13 @@ def tablePrintHits(playlist_id):
 
         # print hits for each video
         for i in sorted(playlist_hits.keys(), reverse=True):
-            print(playlist_hits[i]['name']+"\t\t"+str(playlist_hits[i]['hits_online'])+"\t\t"+str(playlist_hits[i]['hits_video'])+"\t\t\t"+str(playlist_hits[i]['hits_audio']));
+            print(playlist_hits[i]['name'] + "\t\t" + str(playlist_hits[i]['hits_online']) + "\t\t" + str(
+                playlist_hits[i]['hits_video']) + "\t\t\t" + str(playlist_hits[i]['hits_audio']))
 
         print("\n")
     else:
-        print("Error while fetching API. Calling api.getStatisticsByPlaylist("+playlist_id+") returned HTTP-Status-Code: "+playlist_attributes_status)
+        print(
+            "Error while fetching API. Calling api.getStatisticsByPlaylist(" + playlist_id + ") returned HTTP-Status-Code: " + playlist_attributes_status)
 
 
 def printAllStatistics():
@@ -47,14 +48,8 @@ def printAllStatistics():
         tablePrintHits(playlists[i]['id'])
 
 
-
-
-
-
-
-
 def printPDF(playlist_id):
-    print("Trying to export "+playlist_id+"...")
+    print("Trying to export " + playlist_id + "...")
 
     # 1st: get basic attributes of playlist, like name
     playlist_attributes_status = api.getPlaylistAttributes(playlist_id)['status']
@@ -67,7 +62,7 @@ def printPDF(playlist_id):
     # check if API call worked
     if playlist_attributes_status == 200:
         playlist_name = playlist_attributes['name']
-        playlist_url = "https://cast.itunes.uni-muenchen.de/vod/playlists/"+playlist_id+".html"
+        playlist_url = "https://cast.itunes.uni-muenchen.de/vod/playlists/" + playlist_id + ".html"
 
         # 2nd: get number of hits for each video in playlist
         playlist_hits_status = api.getStatisticsByPlaylist(playlist_id)['status']
@@ -75,26 +70,23 @@ def printPDF(playlist_id):
 
         # check if API call worked
         if playlist_hits_status == 200:
-            print("   ...name: "+playlist_name)
-            pdf.printStatistics('export_pdf/'+playlist_name, 'heute ;)', playlist_name, playlist_url, playlist_hits)
+            print("   ...name: " + playlist_name)
+            pdf.printStatistics('export_pdf/' + playlist_name, 'heute ;)', playlist_name, playlist_url, playlist_hits)
         else:
-            print("Error while fetching API. Calling api.getStatisticsByPlaylist("+playlist_id+") returned HTTP-Status-Code: "+playlist_attributes_status)
+            print(
+                "Error while fetching API. Calling api.getStatisticsByPlaylist(" + playlist_id + ") returned HTTP-Status-Code: " + playlist_attributes_status)
 
     else:
-        print("Error while fetching API. Calling api.getPlaylistAttributes("+playlist_id+") returned HTTP-Status-Code: "+playlist_attributes_status)
-
-
-
-
+        print(
+            "Error while fetching API. Calling api.getPlaylistAttributes(" + playlist_id + ") returned HTTP-Status-Code: " + playlist_attributes_status)
 
 
 def printAllPDF():
     playlists = api.getPlaylists()['content']
-    print("Going to export "+str(len(playlists))+" PDFs...")
+    print("Going to export " + str(len(playlists)) + " PDFs...")
 
     for i in playlists:
         printPDF(playlists[i]['id'])
-
 
 
 printAllPDF()
